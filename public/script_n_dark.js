c722ff2549b4941ea1272d7ff91e870ce2619bfb
weblikotresk <60964556+weblikotresk@users.noreply.github.com>
2020-09-01 15:54:15 +0300

    Update #14
    localstorage fix, comments

--- public/script_n_dark.js
+++ public/script_n_dark.js
@@ -1,5 +1,4 @@
 
-
 function convertSeconds(seconds){
     let date = new Date();
     date.setTime(seconds * 1000);
@@ -55,8 +54,6 @@ function videoBack(name){
   }
 
 }
-//slidemenu
-
 
 let localization = {
   'ru':{
@@ -171,67 +168,65 @@ let localization = {
   },
 
 };
+
 //Settings 
-let request = new Object();
-// document.cookie = 'lang=ru';
- console.log(localStorage.lang);
-if(localStorage.lang == 'undefined'){
-  request.lang = 'ru';
-  request.units = 'si';
-  request.wind = false;
-}else{
-  request.lang = localStorage.lang;
-  request.units = localStorage.units;
-  request.wind = localStorage.wind;
+//Settings are using localStorage, so we store everything there
+if(localStorage.lang == undefined){
+  //default localStorage
+  localStorage.lang = 'ru';
+  localStorage.units = 'si';
+  localStorage.wind = 'off';
 }
 
 let settingsInputs =[];
 settingsInputs[0] =  document.querySelectorAll('.lang');
 settingsInputs[1] =  document.querySelectorAll('.units');
-settingsInputs[2] =  document.querySelectorAll('.wind');
+settingsInputs[2] =  document.querySelectorAll('.wind-input');
 
   for(let i =0;i<settingsInputs[0].length; i++){
-    if(request.lang == settingsInputs[0][i].value){
+    //we need to highlight options that are in the localStorage
+    if(localStorage.lang == settingsInputs[0][i].value){
       settingsInputs[0][i].checked = true;
     }
     settingsInputs[0][i].addEventListener('click', ()=>{
-      request.lang = settingsInputs[0][i].value;
+      //onclick we change option in the localStorage 
       localStorage.lang = settingsInputs[0][i].value;
     })
   }
   for(let i =0;i<settingsInputs[1].length; i++){
-    if(request.units == settingsInputs[1][i].value){
+    if(localStorage.units == settingsInputs[1][i].value){
       settingsInputs[1][i].checked = true;
     }
     settingsInputs[1][i].addEventListener('click', ()=>{
-      request.units = settingsInputs[1][i].value;
       localStorage.units = settingsInputs[1][i].value;
     })
   }
-  for(let i =0;i<settingsInputs[2].length; i++){
-    if(request.wind == settingsInputs[2][i].value){
-      settingsInputs[2][i].checked = true;
-    }
-    settingsInputs[2][i].addEventListener('click', ()=>{
-      request.wind = settingsInputs[2][i].value;
-      localStorage.wind = settingsInputs[2][i].value;
-    })
-  }
+   for(let i =0;i<settingsInputs[2].length; i++){
+
+     if(localStorage.wind == settingsInputs[2][i].value){
+       settingsInputs[2][i].checked = true;
+     }
+     settingsInputs[2][i].addEventListener('click', ()=>{
+       localStorage.wind = settingsInputs[2][i].value;
+     })
+   }
 
 
 let settingsIcon = document.querySelector('#settings > svg'),
 closeBtn = document.getElementsByClassName('close_settings')[0];
 settingsIcon.addEventListener('click', ()=>{
+  //open the settings window
   document.getElementsByClassName('settings_window')[0].style.zIndex = 2;
   document.getElementsByClassName('settings_window')[0].style.clipPath = 'circle(100%)';
 });
 
 closeBtn.addEventListener('click', ()=>{
+  //close the settings window
     document.getElementsByClassName('settings_window')[0].style.clipPath = 'circle(0.1% at 53.6% 2.2%)'; 
-    setTimeout(()=>{document.getElementsByClassName('settings_window')[0].style.zIndex = -2;load(request)}, 550);
+    setTimeout(()=>{document.getElementsByClassName('settings_window')[0].style.zIndex = -2;load(localStorage)}, 550);
 });
 
-//Loading
+//Loading window
 let load_text = document.getElementsByClassName('loading_support');
 load_text[0].innerHTML = localization['ru'].loading;
 load_text[1].innerHTML = localization['en'].loading;
@@ -239,30 +234,28 @@ setTimeout(()=>{
 if(load_text[0] != undefined){
     load_text[0].innerHTML = localization['ru'].not_load;
     load_text[1].innerHTML = localization['en'].not_load;
-    let reload_btn = document.createElement('div');
-    reload_btn.classList.add('reload_btn');
-    
-  
 }}, 5000);
 
 
 window.onload = load();
+//we set default parameter for the load function as localStorage default values,
+//if function runs for the first time, else we load there 
+//values that were chosen earlier and were put in the localStorage
+function load(request_data = localStorage){
 
-function load(request_data = request){
-
-  
     if('geolocation' in navigator){
         navigator.geolocation.getCurrentPosition(async position =>{
           //location btn fix
-         
-          if(request != request_data){
-            request_data = request;
+          if(localStorage != request_data){
+            request_data = localStorage;
           }
+          //setting vars that will go to the api
            let lat = position.coords.latitude,
            lon = position.coords.longitude,
            lang = request_data.lang,
-           units = request_data.units;
-           console.log(lat, lon, units, lang,request ,request_data);
+           units = request_data.units,
+           wind = request_data.wind;
+           console.log(lat, lon, units, lang,localStorage ,request_data, wind);
            const api_url = `weather/${lat},${lon}/${lang}/${units}`;
            const options = {
             method: "GET",
@@ -276,15 +269,7 @@ function load(request_data = request){
            const response = await fetch(api_url, options);
            const rdata = await response.json();
            console.log(rdata);
-           localStorage.lang = lang;
-           localStorage.units = units;
-           console.log(localStorage);
-
-          //  function handleOrientation(event) {
-          //   var x = event.alpha;  
-          //   document.querySelector('.wind_dir').style.transform = `rotate(${45+x}deg)`;
-          //   alert(event.absolute);
-          // }  
+           //rdata is raw data response from the api
           function compass(){
             let promise = FULLTILT.getDeviceOrientation({ 'type': 'world' });
 
@@ -316,7 +301,7 @@ function load(request_data = request){
             });
           }
           compass();
-          
+          //slidemenu manage which chart to summon onclick on slidemenu
            function slidemenu(value, option){
 
             switch(value){
@@ -337,19 +322,19 @@ function load(request_data = request){
             }
             
           }
+          //radio_events removes events for slidemenu and sets new eventListeners
           function radio_events(option){
             let radio_arr = document.getElementsByClassName('slide-toggle');
              radio_arr[0].checked = true;
             remove_events();
             for(let i = 0; i<radio_arr.length;i++){
-              
               radio_arr[i].addEventListener('click', ()=>{
                 slidemenu(radio_arr[i].value, option)}); 
             }
           }
           function chart_summon(option, option_color, option_back, axis_l, mode){
+            //restart chart
             document.getElementById('myChart').remove();
-          
             let chart_block = document.createElement('canvas');
             chart_block.id = 'myChart';
             document.getElementsByClassName('chart-container')[0].append(chart_block);
@@ -357,8 +342,8 @@ function load(request_data = request){
             //chart
           let chart_data = [],
           max_chart =0,
-          chart_labels = [],
-          tooltip_pos = 'top';
+          chart_labels = [];
+
           if(mode=='' || mode ==0){
             for(let i =0; i<24;i++){
               //current data
@@ -370,9 +355,9 @@ function load(request_data = request){
               document.querySelectorAll('.hour > .pop' )[i].innerHTML = Math.round(rdata.hourly.data[i].precipProbability*100) +'%';
               document.querySelectorAll('.hour > .temp' )[i].innerHTML = Math.round(rdata.hourly.data[i].temperature) +localization[lang].units[units].temp;
               document.querySelectorAll('.hour > .sky' )[i].src = `img/icons/${rdata.hourly.data[i].icon}.svg`;
-          
               document.querySelectorAll('.hour > .time' )[i].innerHTML = convertSeconds(rdata.hourly.data[i].time+rdata.offset*3600);
               chart_labels[i] = convertSeconds(rdata.hourly.data[i].time+rdata.offset*3600);
+              //max_chart later makes chart go higher than it's maximum value
               if(max_chart < chart_data[i]){
                 max_chart = chart_data[i];
               }
@@ -405,7 +390,7 @@ function load(request_data = request){
               }
             }
           }
-          
+          //coef helps max_chart to fix chart maximum
           let coef =0;
           if(max_chart > 70){
             coef = 30;
@@ -413,6 +398,7 @@ function load(request_data = request){
             coef = 3;
             tooltip_pos = 'bottom';
           }
+          //using chart.js library
             let ctx = document.getElementById('myChart').getContext('2d');
             let chart = new Chart(ctx, {
                 type: 'line',
@@ -493,6 +479,7 @@ function load(request_data = request){
             });
             
           }
+          //plugin that lets keep all tooltips always shown
           Chart.pluginService.register({
               beforeRender: function(chart) {
                 if (chart.config.options.showAllTooltips) {
@@ -537,14 +524,16 @@ function load(request_data = request){
                 }
               }
             })
-          
+          //removes events for slidemenu
           function remove_events(){
             let new_nav = document.getElementById('slidemenu');
             let newest_nav = new_nav.cloneNode(true);
             new_nav.remove();
             document.getElementById('display').appendChild(newest_nav);
           }
-           //current
+          //end of function declaration part
+
+           //setting_headlines 
            let settings_headlines = document.querySelectorAll('.wind-subitem_label > h3');
            settings_headlines[0].innerHTML = localization[lang].local;
            settings_headlines[1].innerHTML =localization[lang].uni;
@@ -555,14 +544,14 @@ function load(request_data = request){
            let wind_options = document.querySelectorAll('.turn_wind');
            wind_options[0].innerHTML = localization[lang].on;
            wind_options[1].innerHTML = localization[lang].off;
-          // document.querySelector('html').style.backgroundImage = `url(videos/${rdata.currently.icon}.png)`;
-          
+
+          //legal fix lmao
            if(moment().format('YYYY') == 2020){
             document.querySelector('#legal > p>span').innerHTML = moment().format('YYYY')
            }else{
             document.querySelector('#legal > p> span').innerHTML = '2020-' + moment().format('YYYY');
            }
-
+           //current function delivers rdata to every block that needs it
            let current = function current(){
             videoBack(rdata.currently.icon);
               let icon = document.createElement('img');
@@ -586,61 +575,60 @@ function load(request_data = request){
             document.querySelector('.clouds').innerHTML = localization[lang].clouds+ ': ' +Math.round(rdata.currently.cloudCover*100) + '%';
             document.querySelector('.wind_text > h3').innerHTML = localization[lang].wind+ ': ';
             document.querySelector('.wind_speed').innerHTML = rdata.currently.windSpeed +' '+ localization[lang].units[units].wind_units;
-            if(document.querySelector('#wind-1').checked){
-              
+            //if wind is turned off, we remove deviceorientation
+            // event and set normal mode for the wind arrow
+            if(localStorage.wind == 'on'){
               window.addEventListener('deviceOrientation', compass);
             }else{
-              // document.querySelector('.wind_dir').style.transform = `rotate(${rdata.currently.windBearing-180}deg)`
               window.removeEventListener('deviceorientation', compass);
+              setTimeout(()=>
+              document.querySelector('.wind_dir').style.transform = `rotate(${rdata.currently.windBearing+45}deg)`, 1000);
             }
-            
-     
+          
             document.querySelector('#sunr').innerHTML = localization[lang].sunr  +  convertSeconds(rdata.daily.data[0].sunriseTime +rdata.offset*3600);
             document.querySelector('#suns').innerHTML =localization[lang].suns +  convertSeconds(rdata.daily.data[0].sunsetTime +rdata.offset*3600);
             document.querySelector('#pres').innerHTML =localization[lang].pres + rdata.currently.pressure + localization[lang].units[units].pres_units;
             document.querySelector('#hum').innerHTML =localization[lang].hum+ ': '+ Math.round(rdata.currently.humidity*100) + '%';  
 
             let label_arr = document.querySelectorAll('#slidemenu > label > span');
+            //labels for slidemenu
             label_arr[0].innerHTML = localization[lang].wind;
             label_arr[1].innerHTML = localization[lang].hum;
             label_arr[2].innerHTML = localization[lang].uv;
             label_arr[3].innerHTML = localization[lang].clouds;
             label_arr[4].innerHTML = localization[lang].pop;
             
-
+            //backto button removal
             if(document.getElementsByClassName('backto')[0] != null){
               document.getElementsByClassName('backto')[0].remove();
             }
-            //hourly
+            //hourly forecast
             for(let i =0;i<24;i++){
-              
               let pop = document.querySelectorAll('.hour > .pop' )[i];
               pop.innerHTML =water_drop + Math.round(rdata.hourly.data[i].precipProbability*100)+'%' ;
-              //pop.prepend(water_drop);
+              pop.prepend(water_drop);
               document.querySelectorAll('.hour > .temp' )[i].innerHTML = Math.round(rdata.hourly.data[i].temperature) +localization[lang].units[units].temp;
               document.querySelectorAll('.hour > .sky' )[i].src = `img/icons/${rdata.hourly.data[i].icon}.svg`;
-      
               document.querySelectorAll('.hour > .time' )[i].innerHTML = convertSeconds(rdata.hourly.data[i].time+rdata.offset*3600);
             }
+
             radio_events('');
             chart_summon('windSpeed', 'rgb(0,204,255)', 'rgba(0,204,255, 0.25)', localization[lang].units[units].wind_units, 0);
-            
-            
             document.getElementById('days-fake').checked = true;
-              
            }
+           //when current function ended it's work, we remove loading window
            if(load_text[0] != undefined){
            document.getElementsByClassName('loading')[0].remove();
            }
 
            current();
           
-           //daily
+           //daily forecast
              for(let i = 0;i<8;i++){
             document.querySelectorAll('.day > .pop' )[i].innerHTML = Math.round(rdata.daily.data[i].precipProbability*100) +'%';
             document.querySelectorAll('.day > .sky')[i].src = `img/icons/${rdata.daily.data[i].icon}.svg`;
             document.querySelectorAll('.day > .temp')[i].innerHTML = Math.round(rdata.daily.data[i].temperatureMax) +localization[lang].units[units].temp;
-    
+            //date
             let date = moment(rdata.daily.data[i].time*1000);
             if(document.getElementsByClassName('weekday')[i] != null){
               document.getElementsByClassName('weekday')[i].remove();
@@ -651,11 +639,9 @@ function load(request_data = request){
             
             
             
-            // forecast with hours and graphs
-
-
+            // forecast with hours and graphs on click on the day
             let days = document.getElementsByClassName('day');
-            
+            //onclick backto we go to the current weather
             function backToCurrent(){
               let backto =document.createElement('div');
               backto.className='backto';
@@ -666,8 +652,8 @@ function load(request_data = request){
               }else{
                 document.getElementsByClassName('app_temp')[0].after(backto);
               }
-
             }
+
             for(let i = 0;i<days.length-1;i++){
               days[i].addEventListener('click', ()=>{
                 videoBack(rdata.daily.data[i].icon);
@@ -709,22 +695,17 @@ function load(request_data = request){
                   document.querySelector('#suns').innerHTML =localization[lang].suns +  convertSeconds(rdata.daily.data[i].sunsetTime +rdata.offset*3600);
                   document.querySelector('#pres').innerHTML =localization[lang].pres + rdata.daily.data[i].pressure + localization[lang].units[units].pres_units;
                   document.querySelector('#hum').innerHTML =localization[lang].hum+ ': ' + Math.round(rdata.daily.data[i].humidity*100) + '%';
-                  //hourly
-              radio_events(i);
-              chart_summon('windSpeed', 'rgb(0,204,255)', 'rgba(0,204,255, 0.25)', localization[lang].units[units].wind_units, i);
-              
+
+                radio_events(i);
+                chart_summon('windSpeed', 'rgb(0,204,255)', 'rgba(0,204,255, 0.25)', localization[lang].units[units].wind_units, i);
                   
               }); 
-            
             }
-    
-    
            });
-    
-    
     
        
     }else{
+      //if browser doesn't support geolocation, we make special error notification
       load_text[0].innerHTML = localization['ru'].geo_error;
       load_text[1].innerHTML = localization['en'].geo_error;
     }
