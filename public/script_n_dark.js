7a7cd04dda60309c4237e51402f8663ec64164ec
weblikotresk <60964556+weblikotresk@users.noreply.github.com>
2020-12-27 13:14:34 +0300

    v4.6 | Seasons Check, Cleaning CacheStorage, NewYear fix
    Basically last update before push, therfore I have to save this version

--- public/script_n_dark.js
+++ public/script_n_dark.js
@@ -4,7 +4,29 @@ window.mobileAndTabletCheck = function() {
   (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
   return check;
 };
-
+function seasonsCheck(rdata){
+  let min_temp=100;
+  for(let i=0;i<rdata.daily.data.length;i++){
+    if(rdata.flags.units!='si'){
+      if((rdata.daily.data[i].temperatureHigh-32)*5/9<min_temp){
+        min_temp= (rdata.daily.data[i].temperatureHigh-32)*5/9
+      }
+    }else{
+      if(rdata.daily.data[i].temperatureHigh<min_temp){
+        min_temp=rdata.daily.data[i].temperatureHigh;
+      }
+    }
+  }
+  if(min_temp<15 && min_temp>5){
+    return '-autumn'
+  }else if(min_temp<=5){
+    return '-winter'
+  }else if(min_temp>=15 && min_temp<20){
+    return '-spring'
+  }else{
+    return ''
+  }
+}
 function snowCheck(rdata, level){
   if(rdata.summary == undefined || rdata.summary =="404: Something has gone wrong.."){
     rdata.summary="404: Something has gone wrong.."
@@ -17,7 +39,7 @@ function snowCheck(rdata, level){
   if((level.includes('hourly') || level.includes('daily') ||level.includes('currently'))&&
   (rdata.precipType == 'snow' &&
        (rdata.summary.includes('Туман') || rdata.summary.includes('туман') ||
-       rdata.summary.includes('Fog') ||rdata.summary.includes('fog'))&&
+       rdata.summary.includes('Fog') ||rdata.summary.includes('fog') || rdata.icon=='fog')&&
        rdata.humidity*100<99)
   )
   {
@@ -35,6 +57,8 @@ function fogToSnow(str){
     return str.replace('Туман', 'Снег')
   }else if(str.includes('туман')){
     return str.replace('туман', 'снег');
+  }else{
+    return str.replace('.', ',') + ' возможен снег.'
   }
 }
 function convertSeconds(seconds){
@@ -92,23 +116,13 @@ function windIntensity(wind_speed, lang, units){
     return localization[lang].windIntensity.hurricane 
   }
 }
-function videoBack(name, temp){
+function videoBack(name,rdata){
 
   let date = new Date(),
    time = date.getHours(),
-   month = date.getMonth()+1;
-
-  let postfix;
+   postfix;
   if(name == 'clear-day' || name == 'rain'){
-    if((month >=9)&&(month<11)|| (month==11&& temp>0)){
-      postfix= '-autumn';
-    }else if((month >=11)||(month<=2)&& temp<0){
-      postfix= '-winter';
-    }else if((month >=3)&&(month<=5)){
-      postfix ='-spring';
-    }else{
-      postfix = '-summer';
-    }
+    postfix = seasonsCheck(rdata)
   }else{
     postfix = '';
   }
@@ -534,8 +548,6 @@ settingsInputs[3] =  document.querySelectorAll('.back-input');
       localStorage.back = settingsInputs[3][i].value;
     })
   }
-
-
 
 settingsIcon.addEventListener('click', ()=>{
   //open the settings window
@@ -1527,13 +1539,10 @@ function loaded(request_data = localStorage, cached_coords){
             date_ny = date_ny.format(localization[lang].date);
             console.log(date_ny);
              if(date_ny== '31.12' || date_ny== '12.31' ){
-              videoBack('newYear', option.temperature);
+              videoBack('NewYear', rdata);
              }else{
-              videoBack(sky_icon, option.temperature);
+              videoBack(sky_icon, rdata);
              }
-            
-            
-            
             icon.className='icon';
             let curr_icon = document.createElement('img');
             curr_icon.src='https://d3aid59h00lu28.cloudfront.net/img/icons/water.svg';
@@ -1637,12 +1646,19 @@ function loaded(request_data = localStorage, cached_coords){
             days_counter=8
           }
           let create_days=true;
-          if(8-days_counter==0 && document.querySelector('.day')!=null){
+          console.log(days_counter);
+          if(8-days_counter!=0 && document.querySelector('.day')!=null){
             create_days=false;
           }else{
             create_days=true;
           }
-         
+          let days_length = document.querySelectorAll('.day');
+          days_length=days_length.length;
+          console.log('!!!', days_length);
+          if(create_days){
+            
+            document.querySelector('#days').innerHTML ='<input type="radio" name="days-radio" id="days-fake" class="days_radio" checked="">';
+          }
           for(let i=8-days_counter;i<8;i++){
             let date = moment(rdata.daily.data[i].time*1000);
             let sky_icon= rdata.daily.data[i].icon;
@@ -1653,8 +1669,8 @@ function loaded(request_data = localStorage, cached_coords){
               sky_icon='snow';
             }
             if(create_days){
+              
               if(i!=7){
-                
                 document.querySelector('#days').insertAdjacentHTML('beforeend', `
               <input type="radio" name="days-radio" id="days-${i+1}" class="days_radio">
               <label for="days-${i+1}" class="day">
@@ -1690,6 +1706,7 @@ function loaded(request_data = localStorage, cached_coords){
               if(i==8){
                 break;
               }
+              console.log(i);
               document.querySelectorAll('.day > .pop')[i].innerHTML= `<img src="https://d3aid59h00lu28.cloudfront.net/img/icons/water.svg" class="pop_drop">${Math.round(rdata.daily.data[i].precipProbability*100)}%`;
               document.querySelectorAll('.day > .sky')[i].src=`https://d3aid59h00lu28.cloudfront.net/img/icons/${sky_icon}.svg`;
               document.querySelectorAll('.day > .sky')[i].alt = sky_icon;
@@ -1739,10 +1756,10 @@ function loaded(request_data = localStorage, cached_coords){
             }else{
               document.querySelector('.condition').innerHTML = rdata.daily.data[day_number].summary;
             }
-            if(document.querySelectorAll('.date')[day_number]=='31.12' || document.querySelectorAll('.date')[day_number]=='12.31'){
-              videoBack('newYear', rdata.daily.data[day_number].temperatureMax);
+            if(document.querySelectorAll('.date')[day_number].innerHTML.includes('<br>31.12')==true || document.querySelectorAll('.date')[day_number].innerHTML.includes('<br>12.31')==true ){
+              videoBack('NewYear', rdata);
             }else{
-              videoBack(sky_icon, rdata.daily.data[day_number].temperatureMax);
+              videoBack(sky_icon, rdata);
             };
             
             document.getElementById('slide-item-1').checked = 'checked';
--- public/sw.js
+++ public/sw.js
@@ -9,10 +9,12 @@ self.addEventListener('install', e => {
 self.addEventListener('activate', e => {
   console.log('Service Worker: Activated');
   // Remove unwanted caches
+
   e.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cache => {
+          //removing previous version of cache
           if (cache !== cacheName) {
             console.log('Service Worker: Clearing Old Cache');
             return caches.delete(cache);
@@ -23,6 +25,37 @@ self.addEventListener('activate', e => {
   );
 });
 
+
+ //clean cache every week
+function cleaningCache(){
+  
+  caches.open(cacheName).then(cache => {
+  let cssfile = cache.match('style.css').then(resp=>{return resp}).then(res=>{
+    return res.headers.get('date')
+  });
+  cssfile.then(date_old=>{
+    console.log(date_old);
+    let today = new Date();
+    today=today.getTime();
+    date_old = new Date(date_old);
+    date_old=date_old.getTime();
+    if(today - date_old>=604800000){
+      console.log('!! bruh deletion');
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cache => {
+            //removing previous version of cache
+              console.log('Service Worker: Clearing Old Cache');
+              return caches.delete(cache);
+
+          })
+        );
+      })
+      
+    }
+  })
+});
+}
 // Call Fetch Event
  self.addEventListener('fetch', e => {
    console.log('Service Worker: Fetching');
@@ -31,11 +64,11 @@ self.addEventListener('activate', e => {
        .then(res => {
          // Make copy/clone of response
          const resClone = res.clone();
-         // Open cahce
+         // Open cache
          caches.open(cacheName).then(cache => {
-           // Add response to cache
-           if(!(e.request.url.includes('www.google-analytics.com/')) &&  e.request.url.includes('videos')==false){
-           // console.log(cache,e.request, resClone );
+         
+          // Add response to cache
+           if(!(e.request.url.includes('www.google-analytics.com/')) &&  e.request.url.includes('videos')==false  &&  e.request.url.includes('/weather/')==false &&  e.request.url.includes('api')==false){
             cache.put(e.request, resClone);
            }
          });
@@ -44,7 +77,7 @@ self.addEventListener('activate', e => {
        .catch(err => caches.match(e.request).then(res => res))
    );
  });
- 
+cleaningCache();
 // self.addEventListener('fetch', function(event) {
 //   console.log(event.request.url);
  
