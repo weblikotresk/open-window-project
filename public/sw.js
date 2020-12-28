2e18d217280639b94698b73a8677cfd07ee11a81
weblikotresk <60964556+weblikotresk@users.noreply.github.com>
2020-12-28 13:46:16 +0300

    v4.7 | Cache Update, fast loading speed

--- public/sw.js
+++ public/sw.js
@@ -9,7 +9,6 @@ self.addEventListener('install', e => {
 self.addEventListener('activate', e => {
   console.log('Service Worker: Activated');
   // Remove unwanted caches
-
   e.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
@@ -28,62 +27,74 @@ self.addEventListener('activate', e => {
 
  //clean cache every week
 function cleaningCache(){
-  
   caches.open(cacheName).then(cache => {
-  let cssfile = cache.match('style.css').then(resp=>{return resp}).then(res=>{
-    return res.headers.get('date')
+    let cssfile = cache.match('style.css').then(resp=>{return resp}).then(res=>{
+      if(res!=undefined){
+        return res.headers.get('date')
+      }else{
+        return 0
+      }
+    });
+    cssfile.then(date_old=>{
+      let today = new Date();
+      today=today.getTime();
+      date_old = new Date(date_old);
+      date_old=date_old.getTime();
+      if(today - date_old>=604800000){
+        caches.keys().then(cacheNames => {
+          return Promise.all(
+            cacheNames.map(cache => {
+              //removing previous version of cache
+                console.log('Service Worker: Clearing Old Cache');
+                return caches.delete(cache);
+            })
+          );
+        })
+      }
+    })
   });
-  cssfile.then(date_old=>{
-    console.log(date_old);
-    let today = new Date();
-    today=today.getTime();
-    date_old = new Date(date_old);
-    date_old=date_old.getTime();
-    if(today - date_old>=604800000){
-      console.log('!! bruh deletion');
-      caches.keys().then(cacheNames => {
-        return Promise.all(
-          cacheNames.map(cache => {
-            //removing previous version of cache
-              console.log('Service Worker: Clearing Old Cache');
-              return caches.delete(cache);
-
-          })
-        );
-      })
-      
-    }
-  })
-});
 }
+// Add response to cache
+function getCached(request, resClone){
+  // Open cache
+  caches.open(cacheName).then(cache => {
+    if(!(request.url.includes('www.google-analytics.com/'))  &&  request.url.includes('/weather/')==false &&  request.url.includes('api')==false){
+      // console.log('Service Worker: Caching New Material', request, resClone);
+      //because it gives us 206 code
+      if(request.destination != 'video'){
+        cache.put(request, resClone).catch(err=>{
+          console.error(err, request)
+        });
+      }
+    }
+  });
+}
+
 // Call Fetch Event
- self.addEventListener('fetch', e => {
-   console.log('Service Worker: Fetching');
-   e.respondWith( 
-         fetch(e.request)
-       .then(res => {
-         // Make copy/clone of response
-         const resClone = res.clone();
-         // Open cache
-         caches.open(cacheName).then(cache => {
-         
-          // Add response to cache
-           if(!(e.request.url.includes('www.google-analytics.com/')) &&  e.request.url.includes('videos')==false  &&  e.request.url.includes('/weather/')==false &&  e.request.url.includes('api')==false){
-            cache.put(e.request, resClone);
-           }
-         });
-         return res;
-       })
-       .catch(err => caches.match(e.request).then(res => res))
-   );
- });
+self.addEventListener('fetch', e => {
+  console.log('Service Worker: Fetching');
+  e.respondWith( 
+    caches.match(e.request).then(function(response) {
+      //return cached version of response, if it's not in cahce, then fetch and cache it
+      if(response!=undefined){
+        //if material is in cache
+        return response 
+      }else{
+        return fetch(e.request)
+          .then(res => {
+            // Make copy/clone of response
+            const resClone = res.clone();
+            // 2 - cache request anyway 
+            getCached(e.request, resClone)
+            return res;
+          })
+          .catch(err =>{
+            console.log(err);
+            caches.match(e.request).then(res => res)
+          })
+      }
+    })
+  );
+});
 cleaningCache();
-// self.addEventListener('fetch', function(event) {
-//   console.log(event.request.url);
- 
-//   event.respondWith(
-//     caches.match(event.request).then(function(response) {
-//       return response || fetch(event.request);
-//     })
-//   );
-//  });
+
